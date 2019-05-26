0d651a527b2925f6b9a6cabb116a0b05899f1207
denzakh <den.zakh@gmail.com>
2019-05-26 23:03:47 +0300

    start

---
+++ config.js
@@ -0,0 +1,45 @@
+/* global module */
+
+let config = {
+  'notGetBlocks': [
+    'blocks-demo.html',
+  ],
+  'ignoredBlocks': [
+    'no-js',
+  ],
+  'alwaysAddBlocks': [
+    // 'sprite-svg',
+    // 'sprite-png',
+    // 'object-fit-polyfill',
+  ],
+  'addStyleBefore': [
+    'src/scss/variables.scss',
+    'src/scss/variables-responsive.scss',
+    'src/scss/responsive.scss',
+    'src/scss/mixins/index.scss',
+    'src/scss/common/common.scss',
+    // 'somePackage/dist/somePackage.css', // для 'node_modules/somePackage/dist/somePackage.css',
+  ],
+  'addStyleAfter': [
+    // 'src/scss/common/print.scss',
+  ],
+  'addJsBefore': [
+    // 'somePackage/dist/somePackage.js', // для 'node_modules/somePackage/dist/somePackage.js',
+  ],
+  'addJsAfter': [
+    './script.js',
+  ],
+  'addAssets': {
+    'src/fonts/demo-empty-open-sans.woff2': 'fonts/',
+    'src/img/demo-*.{png,svg,jpg,jpeg}': 'img/',
+    // 'src/favicon/*.{png,ico,svg,xml,webmanifest}': 'img/favicon',
+    // 'node_modules/somePackage/images/*.{png,svg,jpg,jpeg}': 'img/',
+  },
+  'dir': {
+    'src': 'src/',
+    'build': 'build/',
+    'blocks': 'src/blocks/'
+  }
+};
+
+module.exports = config;
